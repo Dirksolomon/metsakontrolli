471d4aaf4b51c5bbffc8eab1c5a62f29c5011d98
Dirksolomon <jukka.holopainen@gmail.com>
2018-07-10 10:57:13 +0300

    GitMainFile

---
+++ mainGIT.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://maps.googleapis.com/maps/api/js?key=INSERTKEY&callback=initMap"async defer></script>
+    <script src="map.js"></script>
+  </head>
+  <body>
+    <div id="map"></div>
+  </body>
+</html>
